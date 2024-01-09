d0f4b85e35347fff7033336794af4d4c2eed2885
Mauricioporto21 <101594942+Mauricioporto21@users.noreply.github.com>
2024-01-08 21:56:36 -0300

    Inimigo asset e script

---
+++ Runner Brasil/Assets/Script/Inimigo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject gameObject = new GameObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
